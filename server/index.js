96f55af99e771eb95d30595c5ca954937f2dd01d
Sachinpathak098 <124699279+Sachinpathak098@users.noreply.github.com>
2025-12-14 18:59:14 +0530

    Fix formatting in server/index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();  
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 // Middleware
 app.use(cors());
